89fa1473ffbdf4ed508d74aa2b64439949a30e5d
lukegirardin <lukegirardin@yelp.com>
2026-08-19 09:25:24 -0400

    Implements all comparison operators

---
+++ examples/comparisons.sol
@@ -0,0 +1,30 @@
+getMedian -> {
+    parameters(
+        a,
+            is value, type Integer,
+        b,
+            is value, type Integer,
+        c,
+            is value, type Integer,
+    )
+    return if {
+        a < b -> if {
+            b < c -> b
+            a <= c -> c
+            else -> a
+        }
+        else -> if {
+            b > c -> b
+            a >= c -> c
+            else -> a
+        }
+    }
+}
+    is function, returns Integer
+
+main -> {
+    result -> getMedian(5, 3, 8)
+        is value, type Integer
+    printInteger(result)
+}
+    is function, returns nothing
