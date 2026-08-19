0c23f87baf34e1bd4fc6898ebf4e23a67b3eaf60
lukegirardin <lukegirardin@yelp.com>
2026-08-19 09:58:43 -0400

    Adds arithmetic operators

---
+++ examples/arithmetic.sol
@@ -0,0 +1,14 @@
+main -> {
+    a -> 10
+        is value, type Integer
+    b -> 3
+        is value, type Integer
+
+    printLine(a + b)
+    printLine(a - b)
+    printLine(a * b)
+    printLine(a / b)
+    printLine(2 + 3 * 4)
+    printLine(a * 2 + b)
+}
+    is function, returns nothing
