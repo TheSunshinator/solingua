34bc74b3a59e100cfaa9421708d80337c9073466
lukegirardin <lukegirardin@yelp.com>
2026-08-19 10:36:52 -0400

    Adds Boolean type and operators

---
+++ examples/booleans.sol
@@ -0,0 +1,19 @@
+main -> {
+    a -> true
+        is value, type Boolean,
+    b -> false
+        is value, type Boolean,
+
+    printLine(a)
+    printLine(b)
+    printLine(a and b)
+    printLine(a or b)
+    printLine(not a)
+    printLine(not b)
+
+    x -> 7
+        is value, type Integer,
+    printLine(x > 5 and x < 10)
+    printLine(x > 10 or x < 3)
+}
+    is function, returns nothing,
