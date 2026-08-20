144e8326c5f48297706e86b055820c1873ebf9f3
lukegirardin <lukegirardin@yelp.com>
2026-08-19 21:29:24 -0400

    Adds for loops

---
+++ examples/loops.sol
@@ -0,0 +1,10 @@
+main -> {
+    for i from 0 to 5 -> {
+        printLine(i)
+    }
+        exclude last,
+
+    for i from 1 to 5 -> printLine(i * i)
+        include last,
+}
+    is function, returns nothing, local scope,
